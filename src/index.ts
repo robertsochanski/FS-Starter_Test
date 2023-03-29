bedbb7f84cb120772879f3f2c07857a7cce51b59
robertsochanski <info@jr-design.co.uk>
2023-03-29 02:18:35 +0100

    Updated name

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
+  const name = 'Robert Junior';
   greetUser(name);
+});
+const button = document.querySelector("#newbutton");
+button.addEventListener("click", function () {
+  const heading = document.querySelector("#Heading-main");
+  heading.textContent = "Freaking Fuxcking Awesome ! YEAH Awesome and Legendary!";
+});
+
+console.log("im gonna make it");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splides = document.querySelectorAll(".splide");
+  splides.forEach(function (splide, index) {
+    var perPage = index == 0 ? 5 : 3;
+    new Splide(splide, {
+      type: "loop",
+      perPage: perPage
+    }).mount();
+  });
 });
